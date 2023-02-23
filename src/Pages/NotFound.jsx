c1d8ee5bdbbf6e4af98520cb82716eaddaf7ce98
amirk1998 <amirk1998.pv@gmail.com>
2023-02-23 12:00:47 +0330

    NotFound Page(Error 404) has been added to App.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Layout from '../Layout/Layout';
+import { Link } from 'react-router-dom';
 
 function NotFound(props) {
   return (
-    <Layout className='mt-8 flex flex-col items-center justify-center w-[100vh] bg-white h-[200px] rounded-lg'>
-      <p className='text-center text-2xl text-red-400'>
-        This Page is not found !!!
-      </p>
-    </Layout>
+    <div className='my-8 flex flex-col items-center justify-center w-[100vh] bg-white h-[200px] rounded-lg'>
+      <p className='text-center text-6xl text-red-500 mb-4'>Error 404</p>
+      <p className='text-center text-2xl'>This Page is not found !!!</p>
+      <Link to='/' className='text-violet-500 hover:text-violet-700 my-2'>
+        Go to Home Page
+      </Link>
+    </div>
   );
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
+import NotFound from './Pages/NotFound';
 import Profile from './Pages/Profile';
 
 const routes = [
   { path: '/', element: <HomePage /> },
   { path: '/about-us', element: <AboutUs /> },
   { path: '/profile', element: <Profile /> },
+  { path: '*', element: <NotFound /> },
 ];
 
 export default routes;
